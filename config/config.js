5bdedd4a132cd7c08bd4575b9d1ebfa95fe8b657
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-09-15 20:50:14 +0500

    make a config object

---
+++ config/config.js
@@ -0,0 +1,15 @@
+var allTheSettings = {
+  coinbaseApiKey: process.env.COINBASE_API_KEY,
+  coinbaseApiSec: process.env.COINBASE_API_SECRET,
+  coinbaseApiUri: process.env.COINBASE_API_URI,
+  coinbaseApiWallet: process.env.COINBASE_API_WALLET,
+  dbDb: process.env.DATABASE_DB,
+  dbHost: process.env.DATABASE_HOST,
+  dbPass: process.env.DATABASE_PASS,
+  dbUrl: process.env.DATABASE_URL,
+  dbUser: process.env.DATABASE_USER,
+  redisUrl: process.env.REDIS_URL,
+  secret: process.env.SECRET
+};
+
+module.exports = allTheSettings;
