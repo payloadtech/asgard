5ed6ab3b063b7c867e904f5b61650dab64018a0a
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-09-19 12:53:14 +0500

    Add multisig wallet code

--- config/wallet.js
+++ config/wallet.js
@@ -1,14 +1,33 @@
-var request = require('request');
-var Coinbase = require('coinbase').Client;
-var coinbaseParams = {
-  'apiKey': process.env.COINBASE_API_KEY,
-  'apiSecret': process.env.COINBASE_API_SECRET,
-  'baseApiUri': process.env.COINBASE_API_URI
-};
-var coinbase = new Coinbase(coinbaseParams);
+//////////////////////////////////////////////////////////////////////////////
+////                          Wallet Sheezzzzz                            ////
+//////////////////////////////////////////////////////////////////////////////
 
-var Account = require('coinbase').model.Account;
-var btcAccount = new Account(coinbase, {
-  'id': process.env.COINBASE_API_WALLET
-});
-module.exports = btcAccount;
+/*
+The wallet hierarchically derives public addresses, and their corresponding
+P2SH wallet addresses. It requires a none in the redis client, called
+walletNonce, which it will pass through to the argument.
+*/
+
+var bitcore = require('bitcore');
+
+
+var Wallet = function(iteration) {
+  var publicKeyRing = [
+    "xpub69FPCcRRKETsYfQrLZGUqPmiXErDh9km2LPjtLE8tJBcQFfKLJN8FXsptwW62Q61CZvDCj81wRXMj4EKmEMKym6sn8WG8GwcbjefV3n8GWq",
+    "xpub67wfAJFVRFFRiNrdftanqxejaa7cqJV2TYefgwMWB37QDtoBt9pyE7jiWtJti2jjYUmUhPZ7aSxR9FKLgCLXJriSTNeegookB42XV2d1eTN",
+    "xpub67wfJNixk7YJEWPmaoqWSqsaBrBuB7QnF4HJg1mowvyZ7DcftfUZomyoBZc3bsnX7UsguvHnJb6mvyUKd4DEY48MZeAZNYsM21Uqp52btGG"
+  ];
+
+  var publicKeys = publicKeyRing.map(function(xPubKey) {
+    var parent = new bitcore.HDPublicKey(xPubKey);
+    var index = "m/2147483647/0/" + iteration;
+    var childKey = parent.derive(index).publicKey;
+    return childKey;
+  });
+
+  var p2shAddress = new bitcore.Address(publicKeys, 2);
+  return p2shAddress;
+};
+
+
+module.exports = Wallet;
