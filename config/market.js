2ece79cac32f636e15ae360872642f6a5745d039
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-10-06 05:41:10 +0500

    Fix bid and ask calculation errors

--- config/market.js
+++ config/market.js
@@ -14,12 +14,48 @@ module.exports.contRate = function() {
   orderBookChannel.bind('data', function(data) {
 
     // Bitstamp's trading fees
-    var netTradeMultiplier = 9975 / 10000; // deducts the .25% trade fee
-    var netWithdrawalMultiplier = 97 / 100; // deducts the 30 withdrawal fee
+    var tradeFee = 25 / 10000; // deducts the .25% trade fee
+    var netWithdrawalMultiplier = (1000 - 30) / 1000; // deduct $30 per $1000
+    var netDepositMultiplier = (1000 + 100) / 1000; // add $100 per $1000
     var profitPercentage = 2 / 100; // deducts the profit for Payload services
     var usd2pkr = 104.5;
 
+    // BEGIN BID PRICE CALCULATION
+    var bidVolBTC = 0; // the volume of bitcoins
+    var bidValBTC = 0; // the value of the bitcoins
+    var bidWBTC; // the weighted price per bitcoin
+    var bidNetPriceUSD;
+    var bidNetPricePKR;
 
+    for (i = 0; i < data.bids.length; i++) {
+      bidVolBTC += parseFloat(data.bids[i][1]);
+      bidValBTC += parseFloat(data.bids[i][0]) * parseFloat(data.bids[i][1]);
+    }
+
+    // volume weighted price per bitcoin
+    bidWBTC = bidValBTC / bidVolBTC;
+
+    // net bid price per bitcoin in USD
+    bidNetPriceUSD =
+      bidWBTC *
+      (1 - tradeFee) *
+      netWithdrawalMultiplier *
+      (1 - profitPercentage);
+
+    // net bid price per bitcoin in PKR
+    bidNetPricePKR = usd2pkr * bidNetPriceUSD;
+
+    log.debug("BID: cumulative volume of bitcoin", bidVolBTC);
+    log.debug("BID: value of bitcoin in USD", bidValBTC);
+    log.debug("BID: value per bitcoin", bidWBTC);
+    log.info("BID: price per bitcoin USD", bidNetPriceUSD);
+    log.info("BID: price per bitcoin PKR", bidNetPricePKR);
+
+    redis.set("bidRateUSD", bidNetPriceUSD);
+    redis.set("bidRatePKR", bidNetPricePKR);
+    // END BID PRICE CALCULATION
+
+    // BEGIN ASK PRICE CALCULATION
     var askVolBTC = 0; // the volume of bitcoins
     var askValBTC = 0; // the value of the bitcoins
     var askWBTC; // the weighted price per bitcoin
@@ -37,21 +73,22 @@ module.exports.contRate = function() {
     // net ask price per bitcoin in USD
     askNetPriceUSD =
       askWBTC *
-      netTradeMultiplier *
-      netWithdrawalMultiplier *
-      (1 - profitPercentage);
+      (1 + tradeFee) *
+      netDepositMultiplier *
+      (1 + profitPercentage);
 
-    // net ask price per bitcoin in PKR  
+    // net ask price per bitcoin in PKR
     askNetPricePKR = usd2pkr * askNetPriceUSD;
 
     log.debug("ASK: cumulative volume of bitcoin", askVolBTC);
     log.debug("ASK: value of bitcoin in USD", askValBTC);
     log.debug("ASK: value per bitcoin", askWBTC);
-    log.debug("ASK: price per bitcoin USD", askNetPriceUSD);
-    log.debug("ASK: price per bitcoin PKR", askNetPricePKR);
+    log.info("ASK: price per bitcoin USD", askNetPriceUSD);
+    log.info("ASK: price per bitcoin PKR", askNetPricePKR);
 
-    redis.set("rateUSD", askNetPriceUSD);
-    redis.set("ratePKR", askNetPricePKR);
+    redis.set("askRateUSD", askNetPriceUSD);
+    redis.set("askRatePKR", askNetPricePKR);
+    // END ASK PRICE CALCULATION
 
   });
 };
