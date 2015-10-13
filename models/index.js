3511818dd2811d56a9adc2072e4c00608e6f09db
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-10-13 22:44:44 +0500

    Enable SSL for postgre connection

--- models/index.js
+++ models/index.js
@@ -2,8 +2,12 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
-//var config = require('../config/sequelize.json')[env];
-var sequelize = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres' });
+var sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
+});
 var db = {};
 
 sequelize
