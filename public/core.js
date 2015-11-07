dac3f6ab2a77e81e5afa6a47ce91006088d25145
Waleed Ashraf <waleedashraf@outlook.com>
2015-11-07 20:17:19 +0500

    routes update

--- public/core.js
+++ public/core.js
@@ -5,23 +5,23 @@ function mainController($scope, $http) {
     $scope.formData = {};
     // when landing on the page, get all todos and show them
     $http.get('/invoice')
-        .success(function(data) {
+        .success(function (data) {
             $scope.invoices = data;
             console.log(data);
         })
-        .error(function(data) {
+        .error(function (data) {
             console.log('Error: ' + data);
         });
 
     // when submitting the add form, send the text to the node API
-    $scope.createTodo = function() {
+    $scope.createTodo = function () {
         $http.post('/invoice', $scope.formData)
-            .success(function(data) {
+            .success(function (data) {
                 $scope.formData = {}; // clear the form so our user is ready to enter another
                 $scope.todos = data;
                 console.log(data);
             })
-            .error(function(data) {
+            .error(function (data) {
                 console.log('Error: ' + data);
             });
     };
@@ -30,12 +30,23 @@ function ledgerController($scope, $http) {
     $scope.formData = {};
     // when landing on the page, get all todos and show them
     $http.get('/ledger')
-        .success(function(data) {
+        .success(function (data) {
             $scope.ledger = data;
             console.log(data);
         })
-        .error(function(data) {
+        .error(function (data) {
             console.log('Error: ' + data);
         });
-    };
+}
+function transactionController($scope, $http) {
+    $scope.formData = {};
+    // when landing on the page, get all todos and show them
+    $http.get('/transaction')
+        .success(function (data) {
+            $scope.ledger = data;
+            console.log(data);
+        })
+        .error(function (data) {
+            console.log('Error: ' + data);
+        });
 }
