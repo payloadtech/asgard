6d2a1fec941786fb39a3c2bbb775b54f5fb8d535
AqibHameed <aqibpadana@gmail.com>
2015-11-26 11:44:15 +0500

    ledger flow chart implement

--- models/Ledgers.js
+++ models/Ledgers.js
@@ -1,5 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
-    var Ledgers = sequelize.define('Ledgers', {
+    var Invoice = sequelize.define('Invoice', {
         // amount in bitcoin
         amount: {
             type: DataTypes.FLOAT,
@@ -49,11 +49,124 @@ module.exports = function (sequelize, DataTypes) {
         // bank ref
         bankref: {
              type: DataTypes.STRING
-        },   
+        },
         // description
         description: {
             type: DataTypes.STRING(200)
         }
     });
-    return Ledgers;
+    var Merchant = sequelize.define('Merchant', {
+        // amount in bitcoin
+        amount: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        // amount came from
+        from: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        // amount sent to
+        to: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        // transaction id
+        refId: {
+             type: DataTypes.STRING,
+        },
+        // currency of price
+        currency: {
+            type: DataTypes.STRING,
+            defaultValue: "PKR",
+            isIn: [["PKR", "USD", "BTC"]],
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        // units of currency per bitcoin
+        rate: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        // bank ref
+        bankref: {
+             type: DataTypes.STRING
+        },
+        // description
+        description: {
+            type: DataTypes.STRING(200)
+        }
+    });
+    var Exchange = sequelize.define('Exchange', {
+        // amount in PKR
+        amount: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        // amount came from
+        from: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        // amount sent to
+        to: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        // transaction id
+        refId: {
+             type: DataTypes.STRING,
+        },
+        // currency of price
+        currency: {
+            type: DataTypes.STRING,
+            defaultValue: "PKR",
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        // units of currency per bitcoin
+        rate: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        // bank ref
+        bankref: {
+             type: DataTypes.STRING
+        },
+        // description
+        description: {
+            type: DataTypes.STRING(200)
+        }
+    });
+    Merchant.hasMany(Invoice, {as: 'Invoice'});
+    Exchange.hasMany(Merchant,{as: 'Merchant'});
+    return Exchange;
 };
